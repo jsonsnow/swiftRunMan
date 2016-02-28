57068c3a87aaa2f08e02344665f2ed52ed6474d8
json snow <jsonsnow@163.com>
2016-02-28 09:48:22 +0800

    create a swift project

---
+++ swiftRunMan/swiftRunMan/Class/LogInAndRegister/Controllers/RMLoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RMLoginViewController.swift
+//  swiftRunMan
+//
+//  Created by Aspmcll on 16/2/28.
+//  Copyright © 2016年 Aspmcll. All rights reserved.
+//
+
+import UIKit
+
+class RMLoginViewController: UIViewController {
+
+    @IBOutlet weak var userNameLabel: UITextField!
+    
+    @IBOutlet weak var userPassWordLabel: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func clickLogInBtn(sender: AnyObject) {
+        
+        
+    }
+    
+    
+    
+}
---
+++ swiftRunMan/swiftRunMan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftRunMan
+//
+//  Created by Aspmcll on 16/2/28.
+//  Copyright © 2016年 Aspmcll. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
